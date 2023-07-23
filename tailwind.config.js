a483b744785872faace67bd3a176884581644d0e
goyalk130 <goyalkaran130@gmail.com>
2023-07-24 03:49:07 +0530

    loader setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,24 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes:{
+        scooter : {
+         '0%':{
+            bottom: "0rem"
+          },
+          '100%':{
+            bottom: "2rem"
+          }
+        }
+      },
+      animation:{
+        scooter:"scooter infinte ease-in-out"
+      },
+      fontFamily:{
+        "fjalla": ['Fjalla One', 'sans-serif']
+      },
       backgroundImage: {
+        "dots":"url('/dots.png')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
